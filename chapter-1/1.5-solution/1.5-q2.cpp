8d5e7f15060e599717903195aa59b9717c178f0a
Saulius Zenkevic <bratr31@gmail.com>
2026-04-19 18:21:47 +0300

    1.5 question 2 solution added

---
+++ chapter-1/1.5-solution/1.5-q2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+	int a = 0, b = 0, c = 0;
+	std::cout << "Enter 3 numbers: ";
+	std::cin >> a >> b >> c;
+	std::cout << "You inputed " << a << ", " << b << " and " << c << std::endl; 
+}
