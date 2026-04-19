c91d7d0f457c36012d08d2055afe53d1ac1e0808
Saulius Zenkevic <bratr31@gmail.com>
2026-04-19 20:50:14 +0300

    Fixed 1.5 solution, added question to the file for less confusion what's the solution about.

--- chapter-1/1.5-solution/1.5-q2.cpp
+++ chapter-1/1.5-solution/1.5-q2.cpp
@@ -1,8 +1,16 @@
+/*
+Q: Ask the user to enter three values. The program should then print these values. Add an appropriate comment above function main().
+The program should match the following output (when run with input values 4, 5, and 6):
+
+Enter three numbers: 4 5 6
+You entered 4, 5, and 6.
+*/
+
 #include <iostream>
 
 int main() {
 	int a = 0, b = 0, c = 0;
 	std::cout << "Enter 3 numbers: ";
 	std::cin >> a >> b >> c;
-	std::cout << "You inputed " << a << ", " << b << " and " << c << std::endl; 
+	std::cout << "You entered " << a << ", " << b << " and " << c << std::endl; 
 }
